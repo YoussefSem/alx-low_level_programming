ce8642eee67f41a1289cad3df665b05ebe8ed6b3
YoussefSem <youssefsemrani5@gmail.com>
2023-04-11 20:10:39 -0700

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,61 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _strlen - It isn't the mountains ahead to climb that wear you out
+ * it is the pebble in your shoe
+ * Return: length
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s != '\0')
+		len++, s++;
+
+	return (len);
+}
+
+/**
+ * argstostr - function that concatenates all
+ * the arguments of your program.
+ * @ac: argc
+ * @av: arguments
+ * Return: pointer to array
+ */
+
+char *argstostr(int ac, char **av)
+{
+	char *s;
+	int len = 0, i, j, k = 0;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		len += _strlen(av[i]);
+	}
+	len += (ac + 1);
+
+	s = malloc(len * sizeof(char));
+
+	if (s == NULL)
+	{
+		free(s);
+		return (NULL);
+	}
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; j < _strlen(av[i]); j++)
+		{
+			s[k++] = av[i][j];
+		}
+		s[k++] = '\n';
+	}
+
+	return (s);
+}

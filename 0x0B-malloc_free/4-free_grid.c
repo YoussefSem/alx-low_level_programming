72a47f52979351c020f78cb4d52e4b08ec892bb1
YoussefSem <youssefsemrani5@gmail.com>
2023-04-11 20:03:10 -0700

    4-free_grid.c

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,7 +4,7 @@
 
 /**
  * free_grid - It's not bragging if you can back it up
- * free_grid - Write a function that frees a 2 dimensional 
+ * free_grid - Write a function that frees a 2 dimensional
  * grid previously created by your alloc_grid function
  * @grid: pointer to 2D array
  * @height: height of array

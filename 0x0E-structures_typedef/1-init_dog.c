4fcfca786be28d6ee0eb596196fef2eb8d249271
YoussefSem <youssefsemrani5@gmail.com>
2023-04-17 16:50:42 -0700

    1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 #include "dog.h"
 
 /**
- * init_dog - A dog is the only thing on earth that loves you more than you love yourself
+ * init_dog - A dog is the only thing on earth
  * @d: a variable of type struct dog
  * @name: member
  * @age: member
